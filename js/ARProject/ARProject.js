fc9a59e6177b623b9b69d754663d7dc34b5af92c
Slade0290 <ly.david@live.fr>
2021-02-12 11:17:15 +0100

    working sphere

---
+++ js/ARProject/ARProject.js
@@ -0,0 +1,144 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroMaterials,
+  ViroNode,
+  ViroAnimations,
+  Viro3DObject,
+  ViroLightingEnvironment,
+  ViroARImageMarker,
+  ViroARTrackingTargets,
+  ViroSphere,
+  ViroSpotLight,
+  ViroQuad,
+} from 'react-viro';
+
+var createReactClass = require('create-react-class');
+
+
+var ARSphere = createReactClass({
+  getInitialState() {
+    return {
+      texture: "white",
+      playAnim: false,
+      animateCar: false,
+      tapWhite: false,
+    }
+  },
+
+  render: function() {
+    return (
+      <ViroARScene>
+
+        <ViroLightingEnvironment source={require('./res/shapes/garage_1k.hdr')}/>
+
+        <ViroARImageMarker target={"sphere"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+          <ViroNode scale={[0, 0, 0]} transformBehaviors={["billboardY"]} animation={{name:this.state.animName, run:this.state.playAnim,}}>
+            
+            <ViroSphere materials={["white_sphere"]}
+              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+              position={[-.2, .25, 0]}
+              onClick={this._selectWhite}
+              animation={{name:"tapAnimation", run:this.state.tapWhite, onFinish:this._animateFinished}}
+              shadowCastingBitMask={0} />
+              
+          </ViroNode>
+
+          <Viro3DObject
+            scale={[-10, -10, -10]}
+            source={require('./res/shapes/object_sphere.obj')}
+            type="OBJ"
+            materials={this.state.texture}
+            onClick={this._toggleButtons}
+            animation={{name:"scaleCar", run:this.state.animateCar,}} />
+
+          <ViroSpotLight
+            innerAngle={5}
+            outerAngle={25}
+            direction={[0,-1,0]}
+            position={[0, 5, 1]}
+            color="#ffffff"
+            castsShadow={true}
+            shadowMapSize={2048}
+            shadowNearZ={2}
+            shadowFarZ={7}
+            shadowOpacity={.7} />
+
+          <ViroQuad
+            rotation={[-90, 0, 0]}
+            position={[0, -0.001, 0]}
+            width={2.5} height={2.5}
+            arShadowReceiver={true} />
+
+        </ViroARImageMarker>
+       
+      </ViroARScene>
+    );
+  },
+  _onAnchorFound() {
+    this.setState({
+      animateCar: true,
+    })
+  },
+  _toggleButtons() {
+    this.setState({
+      animName: (this.state.animName == "scaleUp" ? "scaleDown" : "scaleUp"),
+      playAnim: true
+    })
+  },
+  _selectWhite(){
+    this.setState({
+      texture : "white",
+      tapWhite: true
+    })
+  },
+  _animateFinished(){
+    this.setState({
+      tapWhite: false,
+    })
+  },
+});
+
+ViroMaterials.createMaterials({
+  white: {
+    lightingModel: "PBR",
+  },
+  white_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(231,231,231)",
+  },
+});
+
+ViroARTrackingTargets.createTargets({
+  sphere : {
+    source : require('./res/sphere.png'),
+    orientation : "Up",
+    physicalWidth : 0.165 // real world width in meters
+  },
+  sphere : {
+    source : require('./res/cube.png'),
+    orientation : "Up",
+    physicalWidth : 0.165 // real world width in meters
+  }
+});
+
+ViroAnimations.registerAnimations({
+    scaleUp:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
+                  duration: 500, easing: "bounce"},
+    scaleDown:{properties:{scaleX:0, scaleY:0, scaleZ:0,},
+                  duration: 200,},
+    scaleCar:{properties:{scaleX:.09, scaleY:.09, scaleZ:.09,},
+                  duration: 500, easing: "bounce"},
+    scaleSphereUp:{properties:{scaleX:.8, scaleY:.8, scaleZ:.8,},
+                  duration: 50, easing: "easeineaseout"},
+    scaleSphereDown:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
+                  duration: 50, easing: "easeineaseout"},
+    tapAnimation:[["scaleSphereUp", "scaleSphereDown"],]
+});
+
+module.exports = ARSphere;
